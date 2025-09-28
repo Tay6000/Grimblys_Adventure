19a99a2ed1b6d9bac998ffbeaf4eeda6549c1c15
Tay6000 <Spike.hunterassing+github@gmail.com>
2025-09-28 00:32:00 -0400

    AllButMobs

--- portal.gd
+++ portal.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta) -> void:
-	pass
+	reset_portal()
 	
+func show_portal():
+	$portal_collision.disabled = false
+	$portal_sprite.visible = true
+	
+func reset_portal():
+	$portal_collision.disabled = true
+	$portal_sprite.visible = false
