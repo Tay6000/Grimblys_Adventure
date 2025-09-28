134d459b2481f76be0c0c0809e3b0aebe6f49bdd
Tay6000 <Spike.hunterassing+github@gmail.com>
2025-09-27 20:06:11 -0400

    monster stuff

--- player.gd
+++ player.gd
@@ -1,4 +1,6 @@
 extends CharacterBody2D
+
+var direction = "back"
 @export var base_speed = 400
 @export var base_health = 10
 @export var base_defense = 0
@@ -11,36 +13,51 @@ var defense_upgrades = []
 var speed_buffs = []
 var health_buffs = []
 var defense_buffs = []
+var gold
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	update_defense()
+	update_health()
+	update_speed()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
 	if Input.is_action_pressed("move_up"):
 		velocity.y = -1
+		direction = "back"
 	elif Input.is_action_pressed("move_down"):
 		velocity.y = 1
+		direction = "front"
 	else:
 		velocity.y = 0
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x = 1
+		direction = "right"
 	elif Input.is_action_pressed("move_left"):
 		velocity.x = -1
+		direction = "left"
 	else:
 		velocity.x = 0
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		if velocity.x > 0:
-			$PlayerSprite.play("jimmywalk_forward")
+			$PlayerSprite.play("grimbly_right_walk")
 		elif velocity.x < 0:
-			$PlayerSprite.play("jimmywalk_backward")
+			$PlayerSprite.play("grimbly_left_walk")
+		if velocity.y > 0:
+			$PlayerSprite.play("grimbly_front_walk")
+		elif velocity.y < 0:
+			$PlayerSprite.play("grimbly_back_walk")
 	else:
-		$PlayerSprite.play("idle")
+		$PlayerSprite.play("idle_" + direction)
+		
+	if health == 0 or health < 0:
+		pass
 		
 	move_and_slide()
 
@@ -73,3 +90,14 @@ func update_defense() -> void:
 		var buff = defense_buffs[i]
 		var defense_add = defense * buff
 		defense = defense + defense_add
+		
+func death() -> void:
+	speed_buffs = []
+	defense_buffs = []
+	health_buffs = []
+	#put reset items function here
+	update_defense()
+	update_health()
+	update_speed()
+	position.x = 947
+	position.y = 810
