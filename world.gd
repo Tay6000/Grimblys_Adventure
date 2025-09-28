00d76b6c0f93b5582db44be50adcb86d1c50e73a
Tay6000 <Spike.hunterassing+github@gmail.com>
2025-09-28 03:17:22 -0400

    main

---
+++ world.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var enemies = 0
+var level = 0
+var current_enemies = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	level = 0
+	enemies = 0
+	current_enemies = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
