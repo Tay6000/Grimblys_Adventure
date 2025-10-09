1cf50c8373cc42e07747db64fed46c86e85e2903
Tay6000 <Spike.hunterassing+github@gmail.com>
2025-10-09 15:27:17 -0400

    main

--- buff_1.gd
+++ buff_1.gd
@@ -1,21 +1,10 @@
 extends Button
 
-@onready var hover_texture = load("res://icon.svg")
-@onready var still_texture = load("res://Sprites/eddy.png")
-
 var multiplier : float
 var buff : int
 
-var in_area
-
 func _ready() -> void:
-	in_area = false
-	icon = still_texture
-	
-func _on_mouse_entered() -> void:
-	in_area = true
-	icon = hover_texture
-	
-func _on_mouse_exited() -> void:
-	in_area = false
-	icon = still_texture
+	size.x = 208
+	size.y = 72
+	position.x = 94
+	position.y = 240
